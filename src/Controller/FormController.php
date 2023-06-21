2add71e0da7b84cf3c3f194de255556fcc50fe51
Sándor Kalmár <66329053+axlord1234529@users.noreply.github.com>
2023-06-21 23:12:05 +0200

    Created form

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -2,18 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class FormController extends AbstractController
 {
     #[Route('/')]
-    public function index(): JsonResponse
+    public function show(Environment $twig, Request $request, EntityManagerInterface $entityManager) : Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/FormController.php',
-        ]);
+        $contact = new Contact();
+
+        $form = $this->createForm(ContactFormType::class,$contact);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $entityManager->persist($contact);
+            $entityManager->flush();
+
+            return new Response("Köszönjük szépen a kérdésedet.
+                            Válaszunkkal hamarosan keresünk a megadott e-mail címen.");
+
+        }
+        elseif ($form->isSubmitted() && !$form->isValid())
+        {
+            return new Response("Hiba! Kérjük töltsd ki az
+                            összes mezőt!");
+        }
+
+        return new Response($twig->render('form/show.html.twig',[
+            'contact_form' => $form->createView()
+        ]));
     }
 }
--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -5,7 +5,7 @@ namespace App\Entity;
 use App\Repository\ContactRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-
+use Symfony\Component\Validator\Constraints as Assert;
 #[ORM\Entity(repositoryClass: ContactRepository::class)]
 class Contact
 {
@@ -14,12 +14,15 @@ class Contact
     #[ORM\Column]
     private ?int $id = null;
 
+    #[Assert\NotBlank]
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
+    #[Assert\NotBlank]
     #[ORM\Column(length: 255)]
     private ?string $email = null;
 
+    #[Assert\NotBlank]
     #[ORM\Column(type: Types::TEXT)]
     private ?string $message = null;
 
